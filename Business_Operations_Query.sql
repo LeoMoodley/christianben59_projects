d1927dc7e6c9e8d2b522451e922562636c8d1156
Christian Benedict <christianbenedict59@gmail.com>
2023-02-09 13:50:43 -0700

    Update Business_Operations_Query.sql

--- Business_Operations_Query.sql
+++ Business_Operations_Query.sql
@@ -1,21 +1,63 @@
-SELECT 
-    orders.order_id, 
-    customers.customer_name, 
-    products.product_name, 
-    SUM(order_items.quantity * order_items.price) AS total_sale
-FROM 
-    orders
-    INNER JOIN customers ON orders.customer_id = customers.customer_id
-    INNER JOIN order_items ON orders.order_id = order_items.order_id
-    INNER JOIN products ON order_items.product_id = products.product_id
-WHERE 
-    orders.order_date BETWEEN '2022-10-01' AND '2022-10-31'
-    AND customers.country = 'Canada'
-GROUP BY 
-    orders.order_id, 
-    customers.customer_name, 
-    products.product_name
-ORDER BY 
-    total_sale DESC
-LIMIT 25;
+-- Create the database
+CREATE DATABASE business_db;
+
+-- Use the database
+USE business_db;
+
+-- Create the customers table
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    zip_code VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the employees table
+CREATE TABLE employees (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    role ENUM('admin', 'employee') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the products table
+CREATE TABLE products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the orders table
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    employee_id INT NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
+
+-- Create the order_items table
+CREATE TABLE order_items (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
 
