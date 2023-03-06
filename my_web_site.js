5710487990d765ec602dd2565f66c7a27b9fa9d7
Christian Benedict <christianbenedict59@gmail.com>
2023-03-06 11:48:54 -0700

    Update my_web_site.js

--- my_web_site.js
+++ my_web_site.js
@@ -1,4 +1,20 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Thank you for reaching out!');
+// Smooth scrolling for anchor links
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if( target.length ) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: target.offset().top
+    }, 1000);
+  }
+});
+
+// Change header background on scroll
+$(window).on('scroll', function() {
+  var scrollPosition = $(this).scrollTop();
+  if (scrollPosition > 100) {
+    $('header').addClass('scrolled');
+  } else {
+    $('header').removeClass('scrolled');
+  }
 });
