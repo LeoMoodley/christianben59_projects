15d383b3d824bb5f690471777ce24962b9026b7b
Christian Benedict <christianbenedict59@gmail.com>
2023-03-06 11:40:16 -0700

    Update Business_Operations_Query.sql

--- Business_Operations_Query.sql
+++ Business_Operations_Query.sql
@@ -1,63 +1,56 @@
--- Create the database
-CREATE DATABASE business_db;
-
--- Use the database
-USE business_db;
-
--- Create the customers table
 CREATE TABLE customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(50) NOT NULL,
-    zip_code VARCHAR(10) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(50),
+  email VARCHAR(50) UNIQUE,
+  phone_number VARCHAR(20),
+  shipping_address VARCHAR(200)
 );
 
--- Create the employees table
-CREATE TABLE employees (
-    employee_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    role ENUM('admin', 'employee') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Create the products table
 CREATE TABLE products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  product_id INT PRIMARY KEY,
+  product_name VARCHAR(50),
+  description TEXT,
+  price DECIMAL(10,2),
+  stock_quantity INT
 );
-
--- Create the orders table
 CREATE TABLE orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT NOT NULL,
-    employee_id INT NOT NULL,
-    total_price DECIMAL(10, 2) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+  order_id INT PRIMARY KEY,
+  order_date DATE,
+  customer_id INT,
+  order_status VARCHAR(20),
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
-
--- Create the order_items table
 CREATE TABLE order_items (
-    item_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+  order_id INT,
+  product_id INT,
+  quantity INT,
+  price DECIMAL(10,2),
+  PRIMARY KEY (order_id, product_id),
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
-
+CREATE TABLE shipments (
+  shipment_id INT PRIMARY KEY,
+  order_id INT,
+  shipment_status VARCHAR(20),
+  FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+CREATE TABLE payment_methods (
+  payment_method_id INT PRIMARY KEY,
+  payment_method_name VARCHAR(50),
+  payment_method_type VARCHAR(20)
+);
+CREATE TABLE payments (
+  payment_id INT PRIMARY KEY,
+  payment_date DATE,
+  payment_method_id INT,
+  payment_amount DECIMAL(10,2),
+  FOREIGN KEY (payment_method_id) REFERENCES payment_methods(payment_method_id)
+);
+CREATE TABLE orders_payments (
+  order_id INT,
+  payment_id INT,
+  PRIMARY KEY (order_id, payment_id),
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY (payment_id) REFERENCES payments(payment_id)
+);
