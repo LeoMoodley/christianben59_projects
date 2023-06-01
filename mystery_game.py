825e815511876f3c97fa7eae5e9e4072974868f6
Christian Benedict <christianbenedict59@gmail.com>
2023-06-01 17:42:43 -0600

    Create mystery_game.py

---
+++ mystery_game.py
@@ -0,0 +1,8 @@
+print("It is 1:00 AM, and you are in your bed sound asleep")
+print("\n You suddenly hear and bang coming from downstairs")
+print("\n What do you do? ")
+response = input("Choose one of the following letters A. Stay upstairs B. Go downstairs ")
+if response == "A.":
+    print(" You hear loud foot steps coming closer to your bedroom door")
+elif response == "B.":
+    print("You sneak downstairs and do not find anything. It is quiet")
