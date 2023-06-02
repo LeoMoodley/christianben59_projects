7ec9098b2b277b5b3988c0303d8ccc25b37cd892
Christian Benedict <christianbenedict59@gmail.com>
2023-06-02 17:32:03 -0600

    Update March_madness_predictor.py

--- March_madness_predictor.py
+++ March_madness_predictor.py
@@ -1,17 +1,18 @@
-import pandas as pd
+import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# Loads the CSV data into a pandas DataFrame so it can be manipulated
+# Load the CSV data into a pandas DataFrame
 df = pd.read_csv('March Madness Data/2023 Game Data.csv')
 
-# The 5 categories that we are evaluating 
-selected_features = ['Kenpom adjusted efficiency', 'BARTTORVIK ADJUSTED EFFICIENCY', 
-                     'EFG %', 'DEFENSE, EFG', 'POINTS PER POSSESSION OFFENSE', 
+# The 5 categories that you want to use
+selected_features = ['KENPOM ADJUSTED EFFICIENCY', 'BARTTORVIK ADJUSTED EFFICIENCY', 
+                     'EFG %', 'EFG % DEFENSE', 'POINTS PER POSSESSION OFFENSE', 
                      'POINTS PER POSSESSION DEFENSE']
+print(df.columns)
 
-# Now create a new dataframe with the most valuable feautures
+# Now create a new dataframe with only the selected features
 df_X = df[selected_features]
 
 # Assuming 'target' is the name of your target column
@@ -20,16 +21,16 @@ if 'target' in df.columns:
 else:
     df_y = None  # Set the target variable to None or any appropriate default value
 
-# Split the data into a training set and a test set. This will likely be a statistic and the output of that statistic
+# Split the data into a training set and a test set.
 X_train, X_test, y_train, y_test = train_test_split(df_X, df_y, test_size=0.2, random_state=42)
 
-# Creates an instance of the model
+# Create an instance of the model
 lr = LogisticRegression()
 
-# Trains the model to observe accuracy
+# Train the model
 lr.fit(X_train, y_train)
 
-# Makes predictions on the test set to see how effective it is 
+# Make predictions on the test set
 predictions = lr.predict(X_test)
 
 # Print the accuracy
