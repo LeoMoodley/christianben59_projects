df930d1189f4fcd4c2a587b56223f42ca4946a97
Christian Benedict <christianbenedict59@gmail.com>
2023-06-01 22:33:47 -0600

    Update March_madness_predictor.py

--- March_madness_predictor.py
+++ March_madness_predictor.py
@@ -3,15 +3,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# Load the CSV data into a pandas DataFrame
+# Loads the CSV data into a pandas DataFrame so it can be manipulated
 df = pd.read_csv('March Madness Data/2023 Game Data.csv')
 
-# The 5 categories that you want to use
+# The 5 categories that we are evaluating 
 selected_features = ['Kenpom adjusted efficiency', 'BARTTORVIK ADJUSTED EFFICIENCY', 
                      'EFG %', 'DEFENSE, EFG', 'POINTS PER POSSESSION OFFENSE', 
                      'POINTS PER POSSESSION DEFENSE']
 
-# Now create a new dataframe with only the selected features
+# Now create a new dataframe with the most valuable feautures
 df_X = df[selected_features]
 
 # Assuming 'target' is the name of your target column
@@ -20,16 +20,16 @@ if 'target' in df.columns:
 else:
     df_y = None  # Set the target variable to None or any appropriate default value
 
-# Split the data into a training set and a test set.
+# Split the data into a training set and a test set. This will likely be a statistic and the output of that statistic
 X_train, X_test, y_train, y_test = train_test_split(df_X, df_y, test_size=0.2, random_state=42)
 
-# Create an instance of the model
+# Creates an instance of the model
 lr = LogisticRegression()
 
-# Train the model
+# Trains the model to observe accuracy
 lr.fit(X_train, y_train)
 
-# Make predictions on the test set
+# Makes predictions on the test set to see how effective it is 
 predictions = lr.predict(X_test)
 
 # Print the accuracy
