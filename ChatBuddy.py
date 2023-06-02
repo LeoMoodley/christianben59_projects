99686408cfbdf1bfc05eecfd23bb768b2d4aa332
Christian Benedict <christianbenedict59@gmail.com>
2023-06-02 17:32:48 -0600

    Update ChatBuddy.py

--- ChatBuddy.py
+++ ChatBuddy.py
@@ -1,40 +1,37 @@
-#Allows for natural language processing and pattern recognition
 import nltk
 from nltk.chat.util import Chat, reflections
 
-#A list of patterns and there corresponding responses stored in a list
 pairs = [
     [
-        r"my name is (.*)",
+        r"(?i)my name is (.*)",
         ["Hello %1, How are you today ?",],
     ],
     [
-        r"hi|hey|hello",
+        r"(?i)hi|hey|hello",
         ["Hello", "Hey there",],
     ],
     [
-        r"what is your name ?",
+        r"(?i)what is your name ?",
         ["I am a bot created by you. You can call me whatever you like.",],
     ],
     [
-        r"how are you ?",
+        r"(?i)how are you ?",
         ["I'm doing good. How about You ?",],
     ],
     [
-        r"sorry (.*)",
+        r"(?i)sorry (.*)",
         ["Its alright", "Its OK, never mind",],
     ],
     [
-        r"I am fine",
+        r"(?i)i am fine",
         ["Great to hear that, How can I help you?",],
     ],
 ]
-#Function takes the pairs and responds based on its knowledge of the inputs
-def chatbot():
-    print("Hi, I'm the chatbot you built")
 
-chat = Chat(pairs, reflections)
-chat.converse()
-#initializes the program
+def chatbot():
+    print("Hello there. I am a chatbot built to carry on a conversation based off a variety of human inputs. How are you doing?")
+    chat = Chat(pairs, reflections)
+    chat.converse()
+
 if __name__ == "__main__":
-    chatbot()
+    chatbot() 
