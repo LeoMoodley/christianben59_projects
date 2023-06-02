d2d4889ac893b6458dce9dfdd85f886292e41706
Christian Benedict <christianbenedict59@gmail.com>
2023-06-02 17:44:44 -0600

    Update mystery_game.py

--- mystery_game.py
+++ mystery_game.py
@@ -1,9 +1,36 @@
-def introduction ():
-print("It is 1:00 AM, and you are in your bed sound asleep")
-print("\n You suddenly hear and bang coming from downstairs")
-print("\n What do you do? ")
-response = input("Choose one of the following letters A. Stay upstairs B. Go downstairs ")
-if response == "A.":
-    print(" You hear loud foot steps coming closer to your bedroom door")
-elif response == "B.":
-    print("You sneak downstairs and do not find anything. It is quiet")
+import time
+
+def scenario(description, options):
+    print(description)
+    time.sleep(2)
+
+    response = input("\nWhat do you do?\n" + options).lower()
+
+    while True:
+        if response in ['a', 'b']:
+            return response
+        else:
+            print("\nInvalid response. Please try again.")
+            response = input().lower()
+
+def introduction():
+    response = scenario("It is 1:00 AM, and you are in your bed sound asleep.\nYou suddenly hear a bang coming from downstairs.", "Choose one of the following:\nA. Stay upstairs\nB. Go downstairs")
+
+    if response == 'a':
+        print("\nYou hear loud footsteps coming closer to your bedroom door.")
+        scenario_2()
+    elif response == 'b':
+        print("\nYou sneak downstairs and do not find anything. It is quiet.")
+        scenario_3()
+
+def scenario_2():
+    # Define the second scenario here
+    pass
+
+def scenario_3():
+    # Define the third scenario here
+    pass
+
+# Start the game
+introduction()
+
